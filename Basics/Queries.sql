25a6aab23cf7122b6df64e66f30b6c70919b2adb
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-25 23:51:08 +0530

    Update Queries.sql

--- Basics/Queries.sql
+++ Basics/Queries.sql
@@ -13,3 +13,9 @@ WHERE ID= '1661';
 /*3*/ 
 /* Query a list of District and population in CITY table. */
 SELECT DISTRICT,POPULATION FROM CITY;
+
+
+/*4*/
+/* Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.*/
+SELECT DISTINCT CITY FROM STATION /* DISTINCT IS USED TO REMOVE COMMON TERMS */
+WHERE MOD(ID,2)=0;
