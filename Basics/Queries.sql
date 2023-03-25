034105ce3403ec0b1c9be0291658288e202909f6
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-25 23:35:30 +0530

    Update Queries.sql

--- Basics/Queries.sql
+++ Basics/Queries.sql
@@ -1,12 +1,15 @@
 /* 1 */
 /* Query all columns (attributes) for every row in the CITY table. */
-
 Select * FROM CITY
+
 
 /*2*/
 /* Query all columns for a city in CITY with the ID 1661.
 The CITY table is described in Readme file. */
-
-
 SELECT * FROM CITY 
 WHERE ID= '1661';
+
+
+/*3*/ 
+/* Query a list of District and population in CITY table. */
+SELECT DISTRICT,POPULATION FROM CITY;
