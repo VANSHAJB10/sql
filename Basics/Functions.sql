01479b851a9c70d376ecad9f31de8b48329eda53
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-27 23:29:40 +0530

    Update Functions.sql

--- Basics/Functions.sql
+++ Basics/Functions.sql
@@ -2,9 +2,24 @@
 SELECT COUNT (CITY) -  COUNT(DISTINCT (CITY)) FROM STATION;
 
 
+
 /* GROUP BY() */
 The Group By statement is used for organizing similar data into groups
 The GROUP BY statement groups rows that have the same values into summary rows
 
+Code -->
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country;  
+
+
 
 /* ORDER BY() */
+Used to order the grouped statements in a particular order.
+ORDER BY COUNT(CustomerID) DESC; 
+
+Code-->
+SELECT COUNT(CustomerID), Country
+FROM Customers
+GROUP BY Country
+ORDER BY COUNT(CustomerID) DESC;
