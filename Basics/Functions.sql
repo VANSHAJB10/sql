fd8a783a188191faa78462c54286f789c3071d85
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-27 23:27:53 +0530

    Update Functions.sql

--- Basics/Functions.sql
+++ Basics/Functions.sql
@@ -1,2 +1,10 @@
 /* Count() --> To find numebr of occurrences */
 SELECT COUNT (CITY) -  COUNT(DISTINCT (CITY)) FROM STATION;
+
+
+/* GROUP BY() */
+The Group By statement is used for organizing similar data into groups
+The GROUP BY statement groups rows that have the same values into summary rows
+
+
+/* ORDER BY() */
