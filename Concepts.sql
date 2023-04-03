fbf40ade26ffbd156578c44e18c49e26ed5d4a1b
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-03 23:50:47 +0530

    Update Concepts.sql

--- Concepts.sql
+++ Concepts.sql
@@ -4,3 +4,9 @@
 * A field with a NULL value is one that has been left blank during record creation.
 
 * It is not possible to test for NULL values with comparison operators, such as =, <, or <> ;  'IS NULL' and 'IS NOT NULL' operators are used instead.
+
+
+/* SELECT TOP */
+* The SELECT TOP clause is used to specify the number of records to return.
+
+* SELECT TOP 3 * FROM Customers;  /* Selects the first three records from the "Customers" table */
