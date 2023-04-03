7c62072cc6e3c156e1de5675f038b35d51a7b357
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-03 23:40:39 +0530

    Create Concepts.sql

---
+++ Concepts.sql
@@ -0,0 +1,6 @@
+/* NULL */
+
+* A NULL value is different from a zero value or a field that contains spaces. 
+* A field with a NULL value is one that has been left blank during record creation.
+
+* It is not possible to test for NULL values with comparison operators, such as =, <, or <> ;  'IS NULL' and 'IS NOT NULL' operators are used instead.
