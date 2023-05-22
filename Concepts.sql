86591fd7fe3ec3f661329f156a7560d92ed3d633
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-22 23:17:07 +0530

    Update Concepts.sql

--- Concepts.sql
+++ Concepts.sql
@@ -11,3 +11,6 @@
 
 * SELECT TOP 3 * FROM Customers;  /* Selects the first three records from the "Customers" table */
 * SELECT TOP 50 PERCENT * FROM Customers; /* selects the first 50% of the records from the "Customers" table */
+
+/*  Cloning Tables */
+* There may be a situation when you just want to create an exact copy or clone of an existing table to test or perform something without affecting the original table.
