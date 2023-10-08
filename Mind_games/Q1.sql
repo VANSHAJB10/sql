ffce955e8098bd66718c93bde8d42efa594095d1
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-10-08 22:27:58 +0530

    Update Q1.sql

--- Mind_games/Q1.sql
+++ Mind_games/Q1.sql
@@ -1,1 +1,15 @@
+* """""" __ """""" means focus on this part
 
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+
+
+ANSWER -----> 
+select name from Customer where referee_id != '2' """" or referee_id is null  """"
